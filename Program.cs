928395815347a37eb14868e2d4b2e48e501ba612
Alexandre Marins Cunha de Oliveira <marins.oliveira@gmail.com>
2021-06-10 00:07:44 -0300

    Primeira importação para o git.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using BaseBlazorAdmin.Shared.Services;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseBlazorAdmin
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddHttpClient("ApiName", client =>
+            {
+                client.BaseAddress = new Uri("https://yourapi");
+            }).AddHttpMessageHandler<AuthroizationMessageHandler>();
+            builder.Services.AddTransient<AuthroizationMessageHandler>();
+
+            builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("ApiName"));
+            builder.Services.AddScoped<IGerencialService, GerencialService>();
+            builder.Services.AddMudServices();
+            builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
